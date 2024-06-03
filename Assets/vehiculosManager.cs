b1d4232ed7abae9430cb7221f94bf262b0771fc2
Oliver Safdieh <102385400+BatiOli9@users.noreply.github.com>
2024-06-03 09:05:42 -0300

    Segundo Commit

---
+++ Assets/vehiculosManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vehiculosManager : MonoBehaviour
+{
+    public Vehiculo vehiculoScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Cantidad de Ruedas: " + vehiculoScript.cantRuedas);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
